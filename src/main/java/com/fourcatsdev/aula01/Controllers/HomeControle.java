f27cc6e606d3bd7a78da44b456406b166fcd38c3
Vini0667 <vini04.silva09+github@gmail.com>
2023-01-11 15:29:50 -0400

    Salva a primeira aula

---
+++ src/main/java/com/fourcatsdev/aula01/Controllers/HomeControle.java
@@ -0,0 +1,15 @@
+package com.fourcatsdev.aula01.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeControle {
+    
+    @RequestMapping ("/")
+    public String index (Model model) {
+        model.addAttribute("msBemVindo", "Bem vindo a biblioteca");
+        return "publica-index";
+    }
+}
