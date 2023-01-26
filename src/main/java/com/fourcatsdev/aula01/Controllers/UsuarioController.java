e1730c8c104a99db2eadfeeb43bd2992c8db24ad
Vini0667 <vini04.silva09+github@gmail.com>
2023-01-26 00:03:48 -0400

    Por algum motivo o thymeleaf não funcionou no 'data-confirm-delete' estranho... Ver depois

--- src/main/java/com/fourcatsdev/aula01/Controllers/UsuarioController.java
+++ src/main/java/com/fourcatsdev/aula01/Controllers/UsuarioController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -19,21 +20,34 @@ import jakarta.validation.Valid;
 public class UsuarioController {
 
     @Autowired
-    private UsuarioRepositorio usuarioRepositorio; 
+    private UsuarioRepositorio usuarioRepositorio;
 
-    @GetMapping ("/novo")
-    public String adicionaUsuario (Model model) {
-        model.addAttribute ("usuario", new Usuario());
+    @GetMapping("/novo")
+    public String adicionaUsuario(Model model) {
+        model.addAttribute("usuario", new Usuario());
         return "/publica-cria-usuario";
     }
 
-    @PostMapping ("/salvar")
-    public String salvarUsuario (@Valid Usuario usuario, BindingResult result, RedirectAttributes attributes) {
+    @PostMapping("/salvar")
+    public String salvarUsuario(@Valid Usuario usuario, BindingResult result, RedirectAttributes attributes) {
         if (result.hasErrors()) {
             return "/publica-cria-usuario";
         }
         usuarioRepositorio.save(usuario);
         attributes.addFlashAttribute("mensagem", "Usuário salvo com sucesso");
         return "redirect:/usuario/novo";
+    }
+
+    @RequestMapping("/admin/listar")
+    public String listarUser(Model model) {
+        model.addAttribute("usuarios", usuarioRepositorio.findAll());
+        return "auth/adm/admin-listar-usuario";
+    }
+
+    @GetMapping ("/admin/apagar/{id}")
+    public String deleteUser (@PathVariable ("id") long id, Model model) {
+        Usuario user = usuarioRepositorio.findById (id).orElseThrow(() -> new IllegalArgumentException("ID inválido"));
+        usuarioRepositorio.delete(user);
+        return "redirect:/usuario/admin/listar";
     }
 }
